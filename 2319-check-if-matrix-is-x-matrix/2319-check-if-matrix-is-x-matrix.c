483d1966ff21177ce2098cae90f60e74655c01a6
UnnatiAgarwal08 <2k24.cs1q.2413941@gmail.com>
2026-08-26 16:04:40 +0530

    Time: 0 ms (100%), Space: 11.9 MB (11.59%) - LeetHub

---
+++ 2319-check-if-matrix-is-x-matrix/2319-check-if-matrix-is-x-matrix.c
@@ -0,0 +1,18 @@
+bool checkXMatrix(int** grid, int gridSize, int* gridColSize) {
+    int n = gridSize;
+
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            if(i == j || i + j == n - 1) {
+                if(grid[i][j] == 0)  
+                    return false;
+            }
+            else {
+                if(grid[i][j] != 0) 
+                    return false;
+            }
+        }
+    }
+
+    return true; 
+}
