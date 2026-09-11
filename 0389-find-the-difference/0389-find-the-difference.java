10a139c563cdb51ce530297a6da2a850420b96c4
UnnatiAgarwal08 <2k24.cs1q.2413941@gmail.com>
2026-09-11 21:26:56 +0530

    Time: 13 ms (7.08%), Space: 42.9 MB (84.42%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+    public char findTheDifference(String s, String t) {
+
+        Map<Character, Integer> mp = new HashMap<>();
+
+        for(int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            mp.put(ch, mp.getOrDefault(ch, 0) + 1);
+        }
+
+        for(int i = 0; i < t.length(); i++) {
+            char ch = t.charAt(i);
+
+            if(!mp.containsKey(ch)) {
+                return ch;
+            }
+
+            mp.put(ch, mp.get(ch) - 1);
+
+            if(mp.get(ch) == 0) {
+                mp.remove(ch);
+            }
+        }
+
+        return ' ';
+    }
+}
