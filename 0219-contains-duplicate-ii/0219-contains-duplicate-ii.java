ccc2a7a2714a3730c725aa69399e87e66836e847
UnnatiAgarwal08 <2k24.cs1q.2413941@gmail.com>
2026-09-17 00:24:49 +0530

    Time: 32 ms (29.76%), Space: 104.8 MB (72.66%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -3,15 +3,13 @@ class Solution {
         HashSet<Integer> set = new HashSet<>();
 
         for (int i = 0; i < nums.length; i++) {
-            if (set.contains(nums[i])) {
+            if (set.contains(nums[i]))
                 return true;
-            }
 
             set.add(nums[i]);
 
-            if (set.size() > k) {
+            if (i >= k)
                 set.remove(nums[i - k]);
-            }
         }
 
         return false;
