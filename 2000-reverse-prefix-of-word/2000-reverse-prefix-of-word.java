667c05e35f970a37f74ee1cac8737a4541cb9cf5
UnnatiAgarwal08 <2k24.cs1q.2413941@gmail.com>
2026-09-23 21:56:37 +0530

    Time: 2 ms (17.34%), Space: 43.1 MB (36.83%) - LeetHub

---
+++ 2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String reversePrefix(String word, char ch) {
+        int i=word.indexOf(ch);
+        if(i==-1)
+           return word;
+        
+        Stack<Character> st=new Stack<>();
+        for(int j=0;j<=i;j++)
+        {
+            st.push(word.charAt(j));
+        }
+        String result="";
+        while(!st.empty())
+           result+=st.pop();
+        
+        result+=word.substring(i+1);
+        return result;
+    }
+}
