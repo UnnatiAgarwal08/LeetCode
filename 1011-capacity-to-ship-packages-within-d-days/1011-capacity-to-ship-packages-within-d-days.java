52a4d355379a00073f7c8d91b2261a1dd72c4d4c
UnnatiAgarwal08 <2k24.cs1q.2413941@gmail.com>
2026-09-25 23:15:26 +0530

    Time: 10 ms (79.41%), Space: 50.4 MB (19.92%) - LeetHub

---
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
@@ -0,0 +1,40 @@
+class Solution {
+    public int shipWithinDays(int[] weights, int days) {
+
+        int low = 0;
+        int high = 0;
+
+        for (int weight : weights) {
+            low = Math.max(low, weight);
+            high += weight;
+        }
+
+        while (low <= high) {
+
+            int mid = low + (high - low) / 2;
+
+            int daysNeeded = 1;
+            int currentWeight = 0;
+
+            for (int weight : weights) {
+
+                if (currentWeight + weight <= mid) {
+                    currentWeight += weight;
+                } 
+                else {
+                    daysNeeded++;
+                    currentWeight = weight;
+                }
+            }
+
+            if (daysNeeded <= days) {
+                high = mid - 1;
+            } 
+            else {
+                low = mid + 1;
+            }
+        }
+
+        return low;
+    }
+}
